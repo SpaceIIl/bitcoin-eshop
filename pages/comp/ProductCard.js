d2ee9eaf0341f80fe64bb71cf09c3ec5cab02395
SpaceIIl <hrujan@outlook.com>
2022-04-16 16:20:21 +0100

    master

--- pages/comp/ProductCard.js
+++ pages/comp/ProductCard.js
@@ -6,7 +6,7 @@ const ProductCard = ({ product }) => {
       <Image src={product.image} height={200} width={200} />
       <h4>{product.product}</h4>
       <h5>{product.category}</h5>
-      <p>$ {product.price}</p>
+      <p>{product.price} CZK</p>
       <button>Add to Cart</button>
     </div>
   );
