295e35a70e1ddd3bb8adac44f1a42e5a768594a2
SpaceIIl <hrujan@outlook.com>
2022-04-16 17:08:40 +0100

    master

--- pages/comp/ProductCard.js
+++ pages/comp/ProductCard.js
@@ -1,13 +1,18 @@
 import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cart.slice';
 
 const ProductCard = ({ product }) => {
+
+    const dispatch = useDispatch();
+
   return (
     <div>
       <Image src={product.image} height={200} width={200} />
       <h4>{product.product}</h4>
       <h5>{product.category}</h5>
       <p>{product.price} CZK</p>
-      <button>Add to Cart</button>
+      <button onClick={() => dispatch(addToCart(product))} >Add to Cart</button>
     </div>
   );
 };
